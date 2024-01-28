3c048c795a8b66858fe2ea95ce3ba1c54e42ed61
NIHILncunia <nihil_ncunia@naver.com>
2024-01-28 11:51:16 +0900

    2024 0128 dto 수정

--- src/player/dto/create-pc.dto.ts
+++ src/player/dto/create-pc.dto.ts
@@ -1,6 +1,15 @@
+import { IsNotEmpty } from 'class-validator';
+
 export class CreatePcDto {
+  @IsNotEmpty()
   public campain_id: string;
+
+  @IsNotEmpty()
   public name: string;
+
+  @IsNotEmpty()
   public level: number;
+
+  @IsNotEmpty()
   public class: string;
 }
--- src/session/dto/create-session.dto.ts
+++ src/session/dto/create-session.dto.ts
@@ -1,7 +1,18 @@
+import { IsNotEmpty } from 'class-validator';
+
 export class CreateSessionDto {
+  @IsNotEmpty()
   public campain_id: string;
+
+  @IsNotEmpty()
   public number: number;
+
+  @IsNotEmpty()
   public name: string;
+
+  @IsNotEmpty()
   public gm: string;
+
+  @IsNotEmpty()
   public bonus_pc: string;
 }
